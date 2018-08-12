154ba61ea54f31b40e74a959e38484dad30ccbb1
Whitney Seiler <whitney.seiler@gmail.com>
2018-08-12 14:46:04 -0400

    import assets and build basic file structure

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div >
+      </div>
+    )
+  }
+}
+
+export default App;
